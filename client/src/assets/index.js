c97224569e81e66ccdd1af2cd3889d0e590e0179
Gabyp05 <gabyapd05@gmail.com>
2024-02-12 13:16:14 -0400

    Navbar and some styles

---
+++ client/src/assets/index.js
@@ -0,0 +1,26 @@
+import logo from "./logo.webp";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import trophy from "./Trophy-home.png";
+import activision from "./activision.webp";
+import logitech from "./logitech.webp";
+import playstation from "./playstation.webp";
+import riotgames from "./riotgames.webp";
+import samsung from "./samsung.webp";
+import asterisco from "./asterisco.svg";
+import arrow from "./arrow-up.svg";
+
+
+export {
+  logo,
+  menu,
+  close,
+  trophy,
+  activision,
+  logitech,
+  playstation,
+  riotgames,
+  samsung,
+  asterisco,
+  arrow,
+};
---
+++ client/src/data.js
@@ -0,0 +1,47 @@
+import { activision, logitech, playstation, riotgames, samsung  } from "./assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    name: "Inicio",
+    path: '/'
+  },
+  {
+    id: "games",
+    name: "Juegos",
+    path: '/games'
+  },
+  {
+    id: "leaderboard",
+    name: "Ranking",
+    path: '/leaderboard'
+  },
+  {
+    id: "dev",
+    name: "Dev Team",
+    path: '/devTeam'
+  },
+];
+
+export const partners = [
+  {
+    id: "activision",
+    logo: activision,
+  },
+  {
+    id: "logitech",
+    logo: logitech,
+  },
+  {
+    id: "playstation",
+    logo: playstation,
+  },
+  {
+    id: "riotgames",
+    logo: riotgames,
+  },
+  {
+    id: "samsung",
+    logo: samsung,
+  },
+];
